de257fb890cf8e155c91c6e9c50970cd4a6414b3
Elizabeth Martínez <elizabethmartinezr95@gmail.com>
2025-07-23 16:06:48 -0600

    first commit

---
+++ js/codisionales.js
@@ -0,0 +1,13 @@
+let dinero = prompt("¿cuanto ganas?")
+
+if (dinero <= 400){
+    alert("sueldo basico")
+}else if(dinero <=1000 ){
+    alert("Sueldo normal")
+}else if(dinero <=2000 ){
+    alert("Sueldo elevado")
+}else if(dinero =>5000 ){
+    alert("you are MRBEAST")
+}else{
+    alert("indigente consegi trabajo pobre")
+}
---
+++ js/swit.js
@@ -0,0 +1,20 @@
+let hijos = 3
+
+switch (hijos) {
+
+    case value: 1
+        console.log("gana una pención de 1000 dolares")
+        break;
+    case value: 2
+        console.log("gana una pención de 2000 dolares")
+        break;
+    case value: 3
+        console.log("gana una pención de 4000 dolares")
+        break;
+    case value: 4
+        console.log("gana una pención de 6000 dolares")
+        break;
+    default:
+  console.log("No funciona");
+  break;
+}
